2761651656e89e1faee3867de979e254be934d6f
m-dan12 <m-dan12@ya.ru>
2025-06-08 06:54:41 +0400

    Регистрация + Авторизация //Готовые

---
+++ CreditRiskSystem.Client/Views/RegistrationView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CreditRiskSystem.Client.Views;
+
+public partial class RegistrationView : UserControl
+{
+    public RegistrationView()
+    {
+        InitializeComponent();
+    }
+}
